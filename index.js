a5e9b277c7af2481f044fcb4259b82170c2a253a
chandru vijayakumar <chandru4395393@gmail.com>
2024-03-05 15:17:52 +0530

    last commit

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ import { format } from "date-fns";
 import path from "path";
 
 const app = express();
-const PORT = 80;
+const PORT = 4000;
 
 app.get("/", (req, res) => {
-  let today = format(new Date(), "dd-mm-yyyy-HH-mm-ss");
+  let today = format(new Date(), "dd-mm-yyyy HH-mm-ss");
+  let message = "Generated at: ";
   const filePath = `TimeStamp/${today}.txt`;
-  fs.writeFileSync(filePath, `${today}`, "utf8");
+  fs.writeFileSync(filePath, `${message}${today}`, "utf8");
   let data = readFileSync(filePath, "utf8");
   res.status(200).send(data);
 });
+
 app.get("/getTextFiles", (req, res) => {
   const folderPath = "TimeStamp";
 
@@ -21,13 +23,14 @@ app.get("/getTextFiles", (req, res) => {
       console.log(err);
       res
         .status(500)
-        .send("An error occured while listing the files from directory");
+        .send("An error occurred while listing the files from directory");
     } else {
       const textFiles = files.filter((file) => path.extname(file) === ".txt");
       res.status(200).json(textFiles);
     }
   });
 });
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
-}); //running status
+});
