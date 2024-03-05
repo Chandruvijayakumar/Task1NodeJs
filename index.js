408fff6440f65e985aeb263aab7659dea247e8b2
chandru vijayakumar <chandru4395393@gmail.com>
2024-03-05 14:34:25 +0530

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 import { format } from "date-fns";
 import { send } from "process";
 const app = express();
-const PORT = 4000; //https://localhost:4000
+const PORT = 80; //https://localhost:4000
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: "Welcome to the My API" });
@@ -31,6 +31,7 @@ app.get('/write-read', (req, res) => {
     let data = fs.readFileSync(filePath , 'utf8');
     res.status(200), send()
 })
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 }); //running
@@ -38,3 +39,35 @@ app.listen(PORT, () => {
 // app.use(); // middleware
 // app.get("/");
 //http://localhost:4000/getProducts
+// import express from "express";
+// import fs, { readFileSync } from "fs";
+// import { format } from "date-fns";
+// import path from "path";
+
+// const app = express();
+// const PORT = 4000;
+
+// app.get("/", (req, res) => {
+//   let today = format(new Date(), "dd-mm-yyyy-HH-MM-ss");
+//   const filePath = `TimeStamp/${today}.txt`;
+//   fs.writeFileSync(`filePath, ${today}, 'utf8'`);
+//   let data = readFileSync(filePath, "utf8");
+//   res.status(200).send(data);
+// });
+// app.get("/getTextFiles", (req, res) => {
+//   const folderPath = "TimeStamp";
+//   fs.readdir(folderPath, (err, files) => {
+//     if (err) {
+//       console.log(err);
+//       res
+//         .status(500)
+//         .send("An error occured while listing the files from directory");
+//     } else {
+//       const textFiles = files.filter((file) => path.extname(file) === ".txt");
+//       res.status(200).json(textFiles);
+//     }
+//   });
+// });
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`);
+// }); //running status
